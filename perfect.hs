0c26fd1eb42f4bb9f96d0d0c93d9e4cd0ec9ed61
eshiett1995 <eshiett1995@gmail.com>
2020-03-25 23:15:40 +0100

    Update perfect.hs

--- perfect.hs
+++ perfect.hs
@@ -4,6 +4,13 @@
 -- the [Int] which is the last parameter of the functions is an output, 
 -- this mean the function returns a list of Integers
 perfects :: Int -> [Int]
+
+-- percfects interates through the 'int' param passed to it, starting from 1 to itself as seen '[1..digit]'
+-- for each number it interates through, it check if it is a perfect number,
+-- by passing the number to 'isPerfect' function as seen 'isPerfect (fromIntegral presentNum)'
+-- if the function 'isPerfect' returns true, that means the number being interated is perfect,
+-- and it gets added to the list via 'presentNum |'
+-- at the end of the iteration all elements that are perfect would be added to the list, which is returned back as [Int]
 perfects digit  = [presentNum | presentNum <- [1..digit], isPerfect (fromIntegral presentNum)]
 
 -- 'isPerfect' indicates the name of the function responsible for checking if a number is a perfect number
@@ -11,5 +18,9 @@ perfects digit  = [presentNum | presentNum <- [1..digit], isPerfect (fromIntegra
 -- the Bool which is the return type of the function, that tells if a number is perfect or not 
 isPerfect :: Integer -> Bool
 
--- For i from 1 to n-1 (that is, [1, 2, 3, 4 ... n-1])
+-- this iterates from 1 to a spefic number (n-1) and for each number in the iteration it performs the steps below,
+-- 'n `mod` i' checks if the number is a divisor (mod is a haskell inbuilt function),
+-- if it is a divsor, it would be added to the list using '1|'
+-- 'sum []' sums all the divisors available for number 'n'
+-- 'n == sum []' checks if the sum of the divisors is equals to 'n' and if it is n is a perfect number        
 isPerfect n =  n == sum [i | i <- [1..n-1], n `mod` i == 0]
