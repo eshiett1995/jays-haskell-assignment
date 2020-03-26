3b85d9eddaa75f98343bd28c703695005128bffc
eshiett1995 <eshiett1995@gmail.com>
2020-03-26 01:39:19 +0100

    Update exponent.hs

--- exponent.hs
+++ exponent.hs
@@ -1,6 +1,7 @@
 -- the first line declares the function by giving it a name 'expon',
 -- the function also allows 2 parameters of 'Int' and also a return type of Int
--- the second line which has 'expon x 0 = 1'
+-- the second line which has 'expon x 0 = 1' is the base case,
+-- which ensures the programme terminates via pattern matching
 -- the third line recursively calls the expon method over and over until power equals 0
 expon :: Int -> Int -> Int
 expon x 0 = 1
