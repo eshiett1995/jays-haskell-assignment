6b8f379d8e77ec826f51637707212bf570444c86
eshiett1995 <eshiett1995@gmail.com>
2020-03-26 14:09:33 +0100

    update
    update

--- caesar-cipher.hs
+++ caesar-cipher.hs
@@ -1,12 +1,18 @@
 import Data.Char
 
--- function caesar is created here which takes in some parameters
--- the first parameter Int specifies that the 'caesar' function takes in an integer
--- the second parameter is a string and the last parameter is the return type which is a string
+-- function cipher is created to find the next element in the next nth position to the right of a particular element
+-- the first parameter Int specifies that the 'cipher' function takes in an integer which is the 'nth position'
+-- the second parameter is a Char and the last parameter is the return type which is a Char
 cipher :: Int -> Char -> Char
+
+-- below defines the mode of operation for 'cipher :: Int -> Char -> Char'
+-- each character 'x' type is determined using haskel's inbuit functions (isDigit,isLower,isUpper)
+-- this determines the type of element it is, so as to calculate the shift algorithm. 
+-- (|) works as an if statement
+-- after the calculation the new value is assigned to 'x' and returned back    
 cipher n x
     | isDigit x = chr $ ord '0' + (ord x - ord '0' + n'') `mod` 10
-    | isLower x = chr $ ord 'a' + (ord x - ord 'a' + n' ) `mod` 26
+    | isLower x = chr $ ord 'a' + (ord x - o rd 'a' + n' ) `mod` 26
     | isUpper x = chr $ ord 'A' + (ord x - ord 'A' + n' ) `mod` 26
     | otherwise = x
     where n'  = n `mod` 26 -- for the letters
