b96393b69725092eee39d69a5af3592f649b2627
eshiett1995 <eshiett1995@gmail.com>
2020-03-26 12:32:53 +0100

    Update caesar-cipher.hs

--- caesar-cipher.hs
+++ caesar-cipher.hs
@@ -1,5 +1,8 @@
 import Data.Char
 
+-- function caesar is created here which takes in some parameters
+-- the first parameter Int specifies that the 'caesar' function takes in an integer
+-- the second parameter is a string and the last parameter is the return type which is a string
 cipher :: Int -> Char -> Char
 cipher n x
     | isDigit x = chr $ ord '0' + (ord x - ord '0' + n'') `mod` 10
@@ -9,5 +12,14 @@ cipher n x
     where n'  = n `mod` 26 -- for the letters
           n'' = n `mod` 10 -- for the digits
 
+-- function caesar is created here which takes in some parameters
+-- the first parameter Int specifies that the 'caesar' function takes in an integer
+-- the second parameter is a string and the last parameter is the return type which is a string
 caesar :: Int -> String -> String
+
+-- 'caesar n xs = map (cipher n) xs' defines the mode of operation of the caesar function
+-- a map is an inbuilt haskell function that takes in a function and a list (a string is technically a list of char),
+-- and returns another list constructed by appling a function (the first argument) to all items in a list
+-- the function given to the 'map' is the cipher function which is able 
+-- to find the 'nth' letter to the right of the intitial item
 caesar n xs = map (cipher n) xs
