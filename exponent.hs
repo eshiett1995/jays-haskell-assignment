67729d6c9c51fda9e40acbb52757fd7edf2b394e
eshiett1995 <eshiett1995@gmail.com>
2020-03-26 01:21:19 +0100

    wip

--- exponent.hs
+++ exponent.hs
@@ -1,3 +1,9 @@
+-- the first line declares the function by giving it a name 'expon',
+-- the function also allows 2 parameters of 'Int' and also a return type of Int
+-- the second line which has 'expon x 0 = 1'
+-- the third line recursively calls the expon method over and over until power equals 0
 expon :: Int -> Int -> Int
 expon x 0 = 1
 expon x power = x * (expon x (power-1))
+
+    
--- perfect.hs
+++ perfect.hs
@@ -5,7 +5,7 @@
 -- this mean the function returns a list of Integers
 perfects :: Int -> [Int]
 
--- percfects interates through the 'int' param passed to it, starting from 1 to itself as seen '[1..digit]'
+-- perfects interates through the 'int' param passed to it, starting from 1 to itself as seen '[1..digit]'
 -- for each number it interates through, it check if it is a perfect number,
 -- by passing the number to 'isPerfect' function as seen 'isPerfect (fromIntegral presentNum)'
 -- if the function 'isPerfect' returns true, that means the number being interated is perfect,
@@ -18,9 +18,10 @@ perfects digit  = [presentNum | presentNum <- [1..digit], isPerfect (fromIntegra
 -- the Bool which is the return type of the function, that tells if a number is perfect or not 
 isPerfect :: Integer -> Bool
 
--- this iterates from 1 to a spefic number (n-1) and for each number in the iteration it performs the steps below,
--- 'n `mod` i' checks if the number is a divisor (mod is a haskell inbuilt function),
--- if it is a divsor, it would be added to the list using '1|'
--- 'sum []' sums all the divisors available for number 'n'
--- 'n == sum []' checks if the sum of the divisors is equals to 'n' and if it is n is a perfect number        
+-- this iterates from 1 to a spefic number (n-1) (with the number being interated denoted as 'i')
+-- and for each number in the iteration it performs the steps below,
+-- 'n `mod` i' checks if the number assigned to the variable 'i' is a divisor (mod is a haskell inbuilt function),
+-- if it is a divsor, it would be added to the list using 'i|'
+-- 'sum []' sums all the divisors available for number 'n' (sum is a haskell inbuilt function)
+-- 'n == sum []' checks if the sum of the divisors is equals to 'n' and if it is, n is a perfect number        
 isPerfect n =  n == sum [i | i <- [1..n-1], n `mod` i == 0]
