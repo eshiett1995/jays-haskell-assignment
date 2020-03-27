aebdd250a977bba4460ae09b97ef1994eec8f744
eshiett1995 <eshiett1995@gmail.com>
2020-03-27 10:21:41 +0100

    Update merge.hs

--- merge.hs
+++ merge.hs
@@ -7,10 +7,11 @@ merge [] xs = xs
 merge ys[] = ys
 
 -- this is the function definition which sorts each element when merging
--- it gets the first element of the second list and compares it with the second element of
+-- it gets the first element of the second list and compares it with the first element of
 -- the first list.
 -- if the first element of the second list is lesser it is added to the front of the  list via 'x:'
--- and if it is not then the second element of the first list is added to the front of the list via 'y:'
+-- and if it is not then the first element of the first list is added to the front of the list via 'y:'
+-- this process is recursively repeated till the end
 merge (x:xs) (y:ys)
    | x <y = x :merge xs (y:ys)
    | otherwise = y : merge ys (x:xs)
